9b2350ede03a1960d3d138228c6b84f359c46afe
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2022-03-18 00:26:03 -0300

    myBestUsers :fire:

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+import { Host_IP } from '@env';
+import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+if (__DEV__) {
+  // eslint-disable-next-line no-undef
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it fro
+    .configure({ host: `${Host_IP}` }) // controls connection & communication settings, inform your IP
+    .useReactNative() // add all built-in react native plugins
+    .connect(); // let's connect!
+
+  console.tron = tron;
+
+  tron.clear();
+}
